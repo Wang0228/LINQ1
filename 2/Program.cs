50e5c36ed467a64100af3ca876018f3217454c91
王子豪 <as258652000@yahoo.com>
2023-02-21 15:43:36 +0800

    更改

--- 2/Program.cs
+++ 2/Program.cs
@@ -15,14 +15,10 @@ namespace _2
             
             IOrderedEnumerable<int> list_odd =  numString.Split(',').Where(x => Convert.ToInt32(x) % 2 != 0).Select(int.Parse).ToList().OrderBy(x=>x);
             IOrderedEnumerable<int> list_even = numString.Split(',').Where(x => Convert.ToInt32(x) % 2 == 0).Select(int.Parse).ToList().OrderBy(x=>x);
-            string odd = "", even = "";
-
-            Console.WriteLine("奇數:");
-            foreach(int x in list_odd) { odd+=(x+","); }
-            Console.WriteLine(odd.TrimEnd(','));
-            Console.WriteLine("\n偶數:");
-            foreach (int x in list_even) { even += (x + ","); }
-            Console.WriteLine(even.TrimEnd(','));
+            Console.Write("奇數:");
+            Console.WriteLine(string.Join(",", list_odd));
+            Console.Write("偶數:");
+            Console.WriteLine(string.Join(",", list_even));
         }
     }
 }
