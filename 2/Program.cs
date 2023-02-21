9f48106e7d96e91840947a5279a3afadebbc116c
王子豪 <as258652000@yahoo.com>
2023-02-21 11:53:21 +0800

    加入專案檔案。

---
+++ 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入由逗號隔開的數列");
+            string numString = Console.ReadLine();
+            
+
+            IOrderedEnumerable<int> list_odd = numString.Split(',').Select(x => Convert.ToInt32(x)).Where(x => x % 2 != 0).ToList().OrderBy(x => x);
+            IOrderedEnumerable<int> list_even = numString.Split(',').Select(x => Convert.ToInt32(x)).Where(x => x % 2 == 0).ToList().OrderBy(x => x);
+            string odd = "", even = "";
+
+            Console.WriteLine("奇數:");
+            foreach(int x in list_odd) { odd+=(x+","); }
+            Console.WriteLine(odd.TrimEnd(','));
+            Console.WriteLine("\n偶數:");
+            foreach (int x in list_even) { even += (x + ","); }
+            Console.WriteLine(even.TrimEnd(','));
+        }
+    }
+}
---
+++ 作業/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 作業
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("輸入一串數字由逗號間隔");
+            string user=Console.ReadLine();
+            List<int> list= new List<int>();
+            var ints = user.Split(',').Select(int.Parse).ToList().OrderByDescending(x=>x);
+            string afterOrder="";
+            foreach (int i in ints) { afterOrder += (i + ","); }
+            Console.WriteLine(afterOrder.TrimEnd(','));
+        }
+    }
+}
